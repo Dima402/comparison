85f577b475814dcbac3fe834a1374c4ca8072f70
Dima402 <56645812+Dima402@users.noreply.github.com>
2020-03-18 13:35:56 +0300

    Add files via upload

---
+++ Проверка адреса/Adres.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adress
+{
+    public class Adres
+    {
+        public bool One(bool result)
+        {
+            return result;
+        }
+
+        public bool Two(bool result)
+        {
+            return result;
+        }
+        public bool Three(bool result)
+        {
+            return result;
+        }
+        public bool Four(bool result)
+        {
+            return result;
+        }
+
+    }
+}
---
+++ Проверка адреса/Testing.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Adress.Tests
+{
+    [TestClass]
+    public class Testing
+    {
+        string Ulitsa = "Авиаторов";
+        string path = @"G:\ИС1-31\Учебная практика\Adress\Адреса.txt";
+
+
+
+        [TestMethod]
+
+        public void Exist()
+        {
+            bool result = false;
+
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line == Ulitsa)
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+                
+            }
+
+            Adres c = new Adres();
+            bool expected = true;
+            bool actual = c.One(result);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+
+        public void Status_Street()
+        {
+            bool result = false;
+
+            if ((Ulitsa == "Авиаторов") || (Ulitsa == "Богдановича") || (Ulitsa == "Большая Октябрьская") || (Ulitsa == "Воинова") || (Ulitsa == "Гагарина") || (Ulitsa == "Декабристов") || (Ulitsa == "Депутатская") || (Ulitsa == "Жукова") || (Ulitsa == "Калинина") || (Ulitsa == "Костромская"))
+            {
+                result = true;
+            }
+            Adres c = new Adres();
+            bool expected = true;
+            bool actual = c.Two(result);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+
+        public void Status_Area()
+        {
+            bool result = false;
+
+            if ((Ulitsa == "Богоявленская") || (Ulitsa == "Волкова") || (Ulitsa == "Комсомольская") || (Ulitsa == "Красная") || (Ulitsa == "Карла Маркса") || (Ulitsa == "Мира") || (Ulitsa == "Октябрьская") || (Ulitsa == "Подвойского") || (Ulitsa == "Советская") || (Ulitsa == "Торговая") || (Ulitsa == "Труда") || (Ulitsa == "Челюскинцев") || (Ulitsa == "Юбилейная") || (Ulitsa == "Юности") || (Ulitsa == "Ярославль-Главный"))
+            {
+                result = true;
+            }
+            Adres c = new Adres();
+            bool expected = true;
+            bool actual = c.Three(result);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+
+        public void Status_Avenue()
+        {
+            bool result = false;
+
+            if ((Ulitsa == "Проспект Авиаторов") || (Ulitsa == "Дзержинского") || (Ulitsa == "Ленина") || (Ulitsa == "Ленинградский") || (Ulitsa == "Октября") || (Ulitsa == "Машиностроителей") || (Ulitsa == "Московский") || (Ulitsa == "Толбухина") || (Ulitsa == "Фрунзе"))
+            {
+                result = true;
+            }
+            Adres c = new Adres();
+            bool expected = true;
+            bool actual = c.Four(result);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+
+    }
+}
